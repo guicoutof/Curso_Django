c31e3db2261cd4e011e1acbf8fb3599fbaeadd45
guicoutof <guicoutof@hotmail.com>
2018-10-04 16:56:59 -0300

    django

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from autoslug import AutoSlugField
 # Create your models here.
 
 class Post(models.Model):
 	title = models.CharField(max_length=200)
 	author = models.ForeignKey("auth.User",on_delete=models.CASCADE)
 	text = models.TextField(verbose_name='texto do blog')
+	image = models.ImageField(upload_to='images', null=True, blank=True)
+	slug = AutoSlugField(populate_form='tiltle',unique=True)
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 
